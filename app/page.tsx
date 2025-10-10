d7d88280951aa3665e81d1fd20c57c937a80b3fe
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-10-10 18:38:33 +0530

    Add Helvetica Neue font files and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="text-red text-4xl ">
+    <main className="min-h-screen w-full flex flex-col justify-end items-end px-1 py-12">
       <div>
         {siteNav.map((nav, i) => {
           return (
-            <div key={i}>
-              <Link href={nav.href} className="home-link">
+            <div key={i} className="w-full">
+              <Link
+                href={nav.href}
+                className="lg:text-8xl text-5xl font-semibold font-helvetica-neue text-right w-full block"
+              >
                 {nav.name}
               </Link>
             </div>
