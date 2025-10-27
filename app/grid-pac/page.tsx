8bead6307867f8e4b259b814165490c3bca484ed
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-10-27 23:29:30 +0530

    feat: test css grid system

---
+++ app/grid-pac/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen w-full">
+      <div className="container">
+        <div className="header"></div>
+        <div className="left-middle"></div>
+        <div className="top-right"></div>
+        <div className="logo"></div>
+        <div className="left-down"></div>
+        <div className="right-down"></div>
+      </div>
+    </div>
+  );
+}
