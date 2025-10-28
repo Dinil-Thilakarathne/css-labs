b3c3e0b45d0175b95a8ab9c1e052c67b2b7b477e
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-10-28 16:19:49 +0530

    feat: update grid layout

--- app/grid-pac/page.tsx
+++ app/grid-pac/page.tsx
@@ -1,14 +1,59 @@
+const data = [
+  {
+    src: "/img1.png",
+    alt: "Image 1",
+    className: "left-top",
+  },
+  {
+    src: "/img2.png",
+    alt: "Image 2",
+    className: "left-down",
+  },
+  {
+    src: "/img3.png",
+    alt: "Image 3",
+    className: "right-top",
+  },
+  {
+    src: "/img4.png",
+    alt: "Image 4",
+    className: "right-down",
+  },
+];
+
 export default function Page() {
   return (
     <div className="min-h-screen w-full">
       <div className="container">
         <div className="header"></div>
-        <div className="left-middle"></div>
-        <div className="top-right"></div>
         <div className="logo"></div>
-        <div className="left-down"></div>
-        <div className="right-down"></div>
+        {data.map((item, index) => (
+          <ImageItem
+            src={item.src}
+            alt={item.alt}
+            className={item.className}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
 }
+
+const ImageItem = ({
+  src,
+  alt,
+  className,
+}: {
+  src: string;
+  alt: string;
+  className: string;
+}) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={`object-fill h-full w-full ${className}`}
+    />
+  );
+};
