4dfc991f05a7ae3b702876fc13fec5a715f78bcb
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-02 18:20:38 +0530

    fix: update image src paths to /images in grid-pac page

--- src/routes/grid-pac/+page.svelte
+++ src/routes/grid-pac/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	const data = [
 		{
-			src: '/img1.png',
+			src: '/images/img1.png',
 			alt: 'Image 1',
 			className: 'left-top'
 		},
 		{
-			src: '/img2.png',
+			src: '/images/img2.png',
 			alt: 'Image 2',
 			className: 'left-down'
 		},
 		{
-			src: '/img3.png',
+			src: '/images/img3.png',
 			alt: 'Image 3',
 			className: 'right-top'
 		},
 		{
-			src: '/img4.png',
+			src: '/images/img4.png',
 			alt: 'Image 4',
 			className: 'right-down'
 		}
